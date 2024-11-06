b46e34c6ef01b075a6af907cfddb8e46ca84fe0b
CRYSTAL_CM\ynott <gdcrystal04@gmail.com>
2024-11-06 18:45:59 -0500

    added rough camera zoom function added collider to map water

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    
+    public static float zoom;
+    public float baseZoom;
+    public float maxZoom;
+    public float zoomSpeed;
+
+
+    public bool haveToZoom;
+    public float haveToZoomDur;
+    public float haveToZoomDurBase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        zoom = baseZoom;
+        haveToZoomDur = haveToZoomDurBase;
+        gameObject.GetComponent<Camera>().orthographicSize = zoom;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (haveToZoom){
+            zoom += Time.deltaTime * zoomSpeed * 0.01f;
+            if (zoom >= maxZoom){
+                zoom = maxZoom;
+            }
+        }
+        
+        if (haveToZoom){
+            haveToZoomDur -= Time.deltaTime;
+            if (haveToZoomDur <= 0){
+                haveToZoomDur = haveToZoomDurBase;
+                haveToZoom = false;
+            }
+        }
+
+        gameObject.GetComponent<Camera>().orthographicSize = zoom;
+    }
+}

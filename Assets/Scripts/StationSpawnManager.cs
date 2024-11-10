0acf064484a98837b9fbe0a2b8d546b9618bf7d5
Ethan Wu <ethanrwu@gmail.com>
2024-11-10 16:00:05 -0500

    Pull Request Prep

--- Assets/Scripts/StationSpawnManager.cs
+++ Assets/Scripts/StationSpawnManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class StationSpawnManager : MonoBehaviour
 {
+
+    public GameObject square;
+    public GameObject triangle;
+    public GameObject circle;
+
+    private int spawnTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnTime = 0;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        spawnTime++;
+
+        if (spawnTime % 240 == 0)
+        {
+            int stationType = Random.Range(0, 3);
+            int xPos = Random.Range(-9, 9);
+            int yPos = Random.Range(-4, 4);
+            if (stationType == 0) GameObject.Instantiate(square, new Vector3(xPos, yPos, -1), Quaternion.identity);
+            else if (stationType == 1) GameObject.Instantiate(triangle, new Vector3(xPos, yPos, -1), Quaternion.identity);
+            else if (stationType == 2) GameObject.Instantiate(circle, new Vector3(xPos, yPos, -1), Quaternion.identity);
+        }
     }
 }

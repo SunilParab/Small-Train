c0f1216dc048f73c68a6ea703868869282dd3c8b
Ethan Wu <ethanrwu@gmail.com>
2024-11-13 17:32:13 -0500

    New Prefabs

--- Assets/Scripts/StationSpawnManager.cs
+++ Assets/Scripts/StationSpawnManager.cs
@@ -8,6 +8,13 @@ public class StationSpawnManager : MonoBehaviour
     public GameObject square;
     public GameObject triangle;
     public GameObject circle;
+    public GameObject diamond;
+    public GameObject pie;
+    public GameObject star;
+    public GameObject rhombus;
+    public GameObject plus;
+    public GameObject eye;
+    public GameObject pentagon;
 
     private int spawnTime;
 
@@ -24,12 +31,20 @@ public class StationSpawnManager : MonoBehaviour
 
         if (spawnTime % 240 == 0)
         {
-            int stationType = Random.Range(0, 3);
+            int stationType = Random.Range(0, 10);
             int xPos = Random.Range(-9, 9);
             int yPos = Random.Range(-4, 4);
             if (stationType == 0) GameObject.Instantiate(square, new Vector3(xPos, yPos, -1), Quaternion.identity);
             else if (stationType == 1) GameObject.Instantiate(triangle, new Vector3(xPos, yPos, -1), Quaternion.identity);
             else if (stationType == 2) GameObject.Instantiate(circle, new Vector3(xPos, yPos, -1), Quaternion.identity);
+            else if (stationType == 3) GameObject.Instantiate(diamond, new Vector3(xPos, yPos, -1), Quaternion.identity);
+            else if (stationType == 4) GameObject.Instantiate(pie, new Vector3(xPos, yPos, -1), Quaternion.identity);
+            else if (stationType == 5) GameObject.Instantiate(star, new Vector3(xPos, yPos, -1), Quaternion.identity);
+            else if (stationType == 6) GameObject.Instantiate(rhombus, new Vector3(xPos, yPos, -1), Quaternion.identity);
+            else if (stationType == 7) GameObject.Instantiate(plus, new Vector3(xPos, yPos, -1), Quaternion.identity);
+            else if (stationType == 8) GameObject.Instantiate(eye, new Vector3(xPos, yPos, -1), Quaternion.identity);
+            else if (stationType == 9) GameObject.Instantiate(pentagon, new Vector3(xPos, yPos, -1), Quaternion.identity);
+
         }
     }
 }

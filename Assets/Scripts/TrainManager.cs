d689aa29ff9615b1e364fc0d470a6d0a6a6e0ef9
Ethan Wu <ethanrwu@gmail.com>
2024-12-07 21:53:06 -0500

    Picking up passengers, dropping them off, acceleration, deceleration

--- Assets/Scripts/TrainManager.cs
+++ Assets/Scripts/TrainManager.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Xml.Linq;
 using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static System.Collections.Specialized.BitVector32;
 
 public class TrainManager : MonoBehaviour
 {
@@ -19,6 +23,7 @@ public class TrainManager : MonoBehaviour
     
     //content of train as string
     public List<string> insideTrain = new();
+    private int insideTrainMax = 6;
     public float x;
     public float y;
 
@@ -33,6 +38,12 @@ public class TrainManager : MonoBehaviour
 
     bool reversed; //If the train is going backwards
     bool goingToStop;
+
+    //When nearing a station/leaving a station
+    public bool decelerating = false;
+    public bool accelerating = false;
+    float pickUpPassengersTimer = 1000;
+    public bool leavingStation = false;
 
 
     //Iterating through all the segments in these arrays
@@ -53,6 +64,8 @@ public class TrainManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //At the very start, the stationIndex that is next will always be 1
+        myStation = 1;
 
         lineScript = LineList.reference;
         lineInfos = lineScript.lineList;
@@ -100,73 +113,62 @@ public class TrainManager : MonoBehaviour
 
         distance += speed * Time.deltaTime;
 
-        if (Mathf.Sqrt(Mathf.Pow(curTarget.y-curStart.y,2)+Mathf.Pow(curTarget.x-curStart.x,2)) < distance) {
-            distance -= Mathf.Sqrt(Mathf.Pow(curTarget.y-curStart.y,2)+Mathf.Pow(curTarget.x-curStart.x,2));
+        RecalibrateVariables();
 
-            if (curHalf == 1) {
-                
-                var turningAround = false;
-
-
-                var curSegmentIndex = lineInfos[myLine].LineSegments.IndexOf(curSegment);
-
-                if (curSegmentIndex == -1) {
-
-                }
-
-                //Check if its at the end of the line
-                if (reversed) {
-                    if (curSegmentIndex == 0) {
-                        reversed = false;
-                        turningAround = true;
-                    }
-                } else {
-                    if (lineInfos[myLine].LineSegments.Count - 1 == curSegmentIndex) {
-                        reversed = true;
-                        turningAround = true;
-                    }
-                }
-
-                //TODO Make so that the train decelerates when nearing a station, then when speed is 0, PickupPassengers()
-                //After picking up passengers is complete, accelerate again
-                //speed = speed * 0.5f;
-
-                //Get next segment
-                if (!turningAround) {
-                    if (reversed) {
-
-                        //choose station
-                        myStation = LineList.reference.lineList[myLine].StationsInLine.IndexOf(curSegment.GetComponent<SegmentInfo>().startStation);
-                        curSegment = lineInfos[myLine].LineSegments[curSegmentIndex-1];
-
-                        //grab passengers
-                        //PickupPassengers();
-                    } else {
-
-                        //choose station
-                        myStation = LineList.reference.lineList[myLine].StationsInLine.IndexOf(curSegment.GetComponent<SegmentInfo>().endStation);
-                        curSegment = lineInfos[myLine].LineSegments[curSegmentIndex+1];
-
-                        //grab passengers
-                        //PickupPassengers();
-                    }
-                }
-
-
-                curHalf = 0;
-            } else {
-                curHalf = 1;
-            }
-
-            if (!reversed) {
-                curStart = curSegment.lineRenderer.GetPosition(0+curHalf);
-                curTarget = curSegment.lineRenderer.GetPosition(1+curHalf);
-            } else {
-                curStart = curSegment.lineRenderer.GetPosition(2-curHalf);
-                curTarget = curSegment.lineRenderer.GetPosition(1-curHalf);
+        //Detect distance to next station
+        var nextStation = LineList.reference.lineList[myLine].StationsInLine[myStation]; //TODO: Fix nextStation not being set properly
+        float distanceToNextStation = Vector3.Distance(this.transform.position, nextStation.transform.position);
+        //Debug.Log("Station we're going to: " + myStation + "\tDistance: " + distanceToNextStation + "\tSpeed: " + speed);
+        if (distanceToNextStation <= 1 && !leavingStation)
+        {
+            decelerating = true;
+            accelerating = false;
+        }
+        else if (leavingStation)
+        {
+            if (distanceToNextStation > 1)
+            {
+                leavingStation = false;
             }
         }
 
+        //Decelerating
+        if (decelerating)
+        {
+            speed = (1 - (Time.deltaTime * 0.8f)) * speed;
+        }
+
+        //Picking up passengers
+        if (distanceToNextStation <= 0.005f && !accelerating)
+        {
+            speed = 0;
+            if (pickUpPassengersTimer <= 0)
+            {
+                //Debug.Log("Picking up a passenger");
+                if (!DropOffPassengers())
+                {
+                    if (!PickupPassengers())
+                    {
+                        leavingStation = true;
+                    }
+                }
+                pickUpPassengersTimer = 500;
+            }
+            pickUpPassengersTimer -= Time.deltaTime * 500;
+        } else
+        {
+            pickUpPassengersTimer = 500;
+
+            //Accelerating
+            if (accelerating) speed = Math.Min((1 + (Time.deltaTime * 0.8f)) * speed + 0.001f, 1);
+            if (speed == 1) accelerating = false;
+        }
+
+        if (leavingStation)
+        {
+            decelerating = false;
+            accelerating = true;
+        }
 
         //Move the train in the direction by _distance_ amount
         var directionVector = Vector3.Normalize(curTarget - curStart);
@@ -174,28 +176,125 @@ public class TrainManager : MonoBehaviour
 
     }
 
-    void nextSegment(int a)
-    {/*
-        float startXPos = lineInfos[myLine].LineSegments[0].segments[segmentIndexStart].transform.position.x;
-        float startYPos = lineInfos[myLine].LineSegments[0].segments[segmentIndexStart].transform.position.y;
-        float endXpos = lineInfos[myLine].LineSegments[0].segments[segmentIndexEnd].transform.position.x;
-        float endYpos = lineInfos[myLine].LineSegments[0].segments[segmentIndexEnd].transform.position.y;
+    void RecalibrateVariables()
+    {
+        //If the distance that I've traveled is greater than the length of the half segment...
+        if (Mathf.Sqrt(Mathf.Pow(curTarget.y - curStart.y, 2) + Mathf.Pow(curTarget.x - curStart.x, 2)) < distance)
+        {
+            Debug.Log("Recalibrating Variables");
+            distance -= Mathf.Sqrt(Mathf.Pow(curTarget.y - curStart.y, 2) + Mathf.Pow(curTarget.x - curStart.x, 2));
 
-        xDelta = Mathf.Lerp(startXPos, endXpos, 0.1f);
-        yDelta = Mathf.Lerp(startYPos, endYpos, 0.1f);
+            if (curHalf == 1)
+            {
 
-        */ourSegment++;
+                var turningAround = false;
+
+
+                var curSegmentIndex = lineInfos[myLine].LineSegments.IndexOf(curSegment);
+
+                if (curSegmentIndex == -1)
+                {
+
+                }
+
+                //Check if its at the end of the line
+                if (reversed)
+                {
+                    if (curSegmentIndex == 0)
+                    {
+                        reversed = false;
+                        turningAround = true;
+                    }
+                }
+                else
+                {
+                    if (lineInfos[myLine].LineSegments.Count - 1 == curSegmentIndex)
+                    {
+                        reversed = true;
+                        turningAround = true;
+                    }
+                }
+
+                //Get next segment
+                if (!turningAround)
+                {
+                    if (reversed)
+                    {
+                        //choose station
+                        curSegment = lineInfos[myLine].LineSegments[curSegmentIndex - 1];
+                        myStation = LineList.reference.lineList[myLine].StationsInLine.IndexOf(curSegment.GetComponent<SegmentInfo>().startStation);
+                        Debug.Log(curSegmentIndex-1);
+
+                    }
+                    else
+                    {
+                        //choose station
+                        curSegment = lineInfos[myLine].LineSegments[curSegmentIndex + 1];
+                        myStation = LineList.reference.lineList[myLine].StationsInLine.IndexOf(curSegment.GetComponent<SegmentInfo>().endStation);
+                        Debug.Log(curSegmentIndex+1);
+
+                    }
+                } 
+                else
+                {
+                    if (reversed)
+                    {
+                        //choose station
+                        //curSegment = lineInfos[myLine].LineSegments[curSegmentIndex - 1];
+                        myStation = LineList.reference.lineList[myLine].StationsInLine.IndexOf(curSegment.GetComponent<SegmentInfo>().startStation);
+                        //Debug.Log(curSegmentIndex-1);
+
+                    }
+                    else
+                    {
+                        //choose station
+                        //curSegment = lineInfos[myLine].LineSegments[curSegmentIndex + 1];
+                        myStation = LineList.reference.lineList[myLine].StationsInLine.IndexOf(curSegment.GetComponent<SegmentInfo>().endStation);
+                        //Debug.Log(curSegmentIndex+1);
+
+                    }
+                }
+
+
+                curHalf = 0;
+            }
+            else
+            {
+                curHalf = 1;
+            }
+
+            if (!reversed)
+            {
+                curStart = curSegment.lineRenderer.GetPosition(0 + curHalf);
+                curTarget = curSegment.lineRenderer.GetPosition(1 + curHalf);
+            }
+            else
+            {
+                curStart = curSegment.lineRenderer.GetPosition(2 - curHalf);
+                curTarget = curSegment.lineRenderer.GetPosition(1 - curHalf);
+            }
+        }
     }
 
-    void SlowDown() {
+    bool DropOffPassengers()
+    {
+        var station = LineList.reference.lineList[myLine].StationsInLine[myStation];
+        var stationString = station.GetComponent<PassengerSpawn>().stationString;
+
+        for (int i = 0; i < insideTrain.Count; i++) 
+        {
+            if (insideTrain[i].Equals(stationString))
+            {
+                insideTrain.Remove(insideTrain[i]);
+                return true;
+            }
+        }
+
+        return false;
 
     }
 
-    void SpeedUp() {
-
-    }
-
-    void PickupPassengers() {
+    bool PickupPassengers() {
         //if train arrives and has enough space:
         //if (insideTrain.Add.Count == 6) that means its full
 
@@ -203,6 +302,8 @@ public class TrainManager : MonoBehaviour
 
         //get station its reaching as GameObject
         var station = LineList.reference.lineList[myLine].StationsInLine[myStation];
+        var passengersInNextStationGO = station.GetComponent<PassengerSpawn>().passengersInStationGO;
+        var passengersInNextStationString = station.GetComponent<PassengerSpawn>().passengersInStation;
 
         //myStation does not exist right now!!!
 
@@ -213,23 +314,46 @@ public class TrainManager : MonoBehaviour
         //different line that has passenger's shaped station
         //or
         //if connectedLines[i] has stations[j] with (passengrList[k] == station):
-        
+
+        /*Debug.Log("STATIONS:");
+        for (int i = 0; i < LineList.reference.lineList[myLine].StationsInLine.Count; i++)
+        {
+            Debug.Log(LineList.reference.lineList[myLine].StationsInLine[i].GetComponent<PassengerSpawn>().stationString);
+        }
+        Debug.Log("PASSENGERS WAITING AT OUR STATION:");
+        for (int i = 0; i < LineList.reference.lineList[myLine].StationsInLine.Count; i++)
+        {
+            
+        }*/
+
+
         //loop for lines
         for (int i = 0; i < station.GetComponent<PassengerSpawn>().connectedLines.Count; i++){
-            
+            //Debug.Log(station.GetComponent<PassengerSpawn>().connectedLines.Count);
+
             //loop for stations in that line
-            for (int j = 0; j < LineList.reference.lineList[i].StationsInLine.Count; j ++){
+            for (int j = 0; j < LineList.reference.lineList[station.GetComponent<PassengerSpawn>().connectedLines[i]].StationsInLine.Count; j++){
+                stationString = LineList.reference.lineList[station.GetComponent<PassengerSpawn>().connectedLines[i]].StationsInLine[j].GetComponent<PassengerSpawn>().stationString;
                 
-                //loop for passengers in that station in that line
-                for (int k = 0; k < PassengerListInStationInLine(j,i).Count; k ++){
-                    if (PassengerListInStationInLine(j,i)[k].Equals(stationString)){
-                        
-                        insideTrain.Add(PassengerListInStationInLine(i,j)[k]);
-                        return;
+                //loop for passengers in my station in my line
+                for (int k = 0; k < PassengerListInStationInLine(myStation, myLine).Count; k++){
+                    //Debug.Log("Station we're comparing: " + stationString + "\tPassenger we're comparing: " + PassengerListInStationInLine(myStation, myLine)[k] +
+                        //"\tTotal number of stations in this line: " + LineList.reference.lineList[station.GetComponent<PassengerSpawn>().connectedLines[i]].StationsInLine.Count);
+                    if (PassengerListInStationInLine(myStation, myLine)[k].Equals(stationString) && insideTrain.Count < insideTrainMax){
+                        //Let's pick up this passenger!
+                        //Debug.Log("Picked up passenger " + passengersInNextStationString[k]);
+                        insideTrain.Add(PassengerListInStationInLine(myStation, myLine)[k]);
+                        PassengerListInStationInLine(myStation, myLine).Remove(PassengerListInStationInLine(myStation, myLine)[k]);
+                        Destroy(PassengerListInStationInLineGO(myStation, myLine)[k]);
+                        PassengerListInStationInLineGO(myStation, myLine).Remove(PassengerListInStationInLineGO(myStation, myLine)[k]);
+                        return true;
                     }
                 }
             }
         }
+
+        //We need to get going!
+        return false;
 
         /*
         //get list of passenger shape as string
@@ -261,5 +385,16 @@ public class TrainManager : MonoBehaviour
 
         return list;
     }
+
+    //return list of passenger GameObjects in specific station in specific line
+    public List<GameObject> PassengerListInStationInLineGO(int chooseStation, int chooseLine)
+    {
+
+        var list = LineList.reference.lineList[chooseLine].StationsInLine[chooseStation]
+        .GetComponent<PassengerSpawn>().passengersInStationGO;
+
+        return list;
+    }
+
 
 }
